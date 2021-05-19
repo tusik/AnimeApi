a465dc18f1d2664e4d1bc8536a6c3f3f92376d5d
Image <image@by.cx>
2021-05-19 22:20:28 +0800

    fix response 502 with no file found

--- src/route/api.rs
+++ src/route/api.rs
@@ -5,26 +5,18 @@ pub mod api{
     use warp::http::{Response};
     use std::collections::HashMap;
     use crate::entity::config::config::{ CONFIG};
+    use std::io;
+    use crate::entity::image_detail::image_detail::ImageDetail;
 
     pub fn api_sample() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone{
-        warp::any()
+        warp::get()
             .and(warp::path("image"))
             .and(warp::query::<HashMap<String, String>>())
             .and_then(sample_image)
     }
-    pub fn read_img(){
 
-    }
-    pub async fn sample_image(params: HashMap<String, String>) -> Result<Response<Vec<u8>>, Rejection> {
-        let mut nin:Option<Vec<&str>> = None;
-        match params.get("nin"){
-            None => {}
-            Some(item) => {
-                nin = Some(item.split(",").collect());
-            }
-        }
-        let image = sample_one(params.get("id"),nin).await.unwrap();
-        let md5 = image.md5;
+    pub async fn read_image(img_data: &mut Vec<u8>, image:&ImageDetail, size:Option<&String>) -> Result<usize,usize>{
+        let md5 = image.md5.clone();
         let tmp_string:Vec<&str> = image.file_url.split("/").collect();
         let ext_tmp:Vec<&str> = tmp_string.last().unwrap().split(".").collect();
         let ext = ext_tmp.last().unwrap();
@@ -33,9 +25,8 @@ pub mod api{
             path_prefix =  CONFIG.as_ref().unwrap().system.path.as_str();
         }
         let mut full_name = format!("{}/images_opt/{}/{}_optmized.{}",&path_prefix, &md5[0..2], &md5, ext);
-        match params.get("size"){
-            None => {}
-            Some(mode) => match mode.as_ref(){
+        match size{
+            Some(s)=> match s.as_str(){
                 "preview"=>{
                     if image.width > 150{
                         full_name = format!("{}/images_preview/{}/{}_optmized.{}",&path_prefix,&md5[0..2],&md5,ext);
@@ -48,8 +39,36 @@ pub mod api{
                 }
                 &_ => {}
             }
+            _ => {}
         }
-        let img_data = tokio::fs::read(full_name).await.unwrap();
+        let img_res =  tokio::fs::read(full_name).await;
+        match &img_res {
+            Ok(d)=>{
+                img_data.clone_from_slice(d.as_slice());
+                return Ok(img_data.len());
+            },
+            _ => {}
+        }
+        return Err(0);
+    }
+    pub async fn sample_image(params: HashMap<String, String>) -> Result<Response<Vec<u8>>, Rejection> {
+        let mut nin:Option<Vec<&str>> = None;
+        match params.get("nin"){
+            None => {}
+            Some(item) => {
+                nin = Some(item.split(",").collect());
+            }
+        }
+        let image = sample_one(params.get("id"),nin).await.unwrap();
+        let tmp_string:Vec<&str> = image.file_url.split("/").collect();
+        let ext_tmp:Vec<&str> = tmp_string.last().unwrap().split(".").collect();
+        let ext = ext_tmp.last().unwrap();
+        let mut img_data = vec![];
+
+        while read_image(&mut img_data, &image,params.get("size")).await.is_err() {
+
+        }
+
         let mut content_type = "image/jpeg";
         if *ext=="png"{
             content_type = "image/png";
