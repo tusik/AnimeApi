643896e016ec1fcfd298568655a5fff059811aa7
Image <image@by.cx>
2023-12-18 15:00:50 +0800

    fix depend issue

--- src/route/main_page.rs
+++ src/route/main_page.rs
@@ -1,3 +1,13 @@
+/*
+ * @Author: Image image@by.cx
+ * @Date: 2021-06-25 08:51:37
+ * @LastEditors: Image image@by.cx
+ * @LastEditTime: 2023-12-18 14:26:12
+ * @filePathColon: /
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by Image, All Rights Reserved. 
+ */
 pub mod main_page{
     use warp::{Filter,reply};
     use rust_embed::RustEmbed;
@@ -9,7 +19,7 @@ pub mod main_page{
 
     pub fn index_static()-> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone{
         warp::path::end().map(||{
-            let index = Asset::get("index.html").unwrap();
+            let index = Asset::get("index.html").unwrap().data;
             reply::html(index)
         })
     }
@@ -18,7 +28,7 @@ pub mod main_page{
             .and(warp::path("css"))
             .and(warp::path("styles.css"))
             .map(||{
-            let css = Asset::get("assets/css/styles.css").unwrap();
+            let css = Asset::get("assets/css/styles.css").unwrap().data;
             reply::html(css)
         }).with(reply::with::header(header::CONTENT_TYPE, "text/css"))
     }
