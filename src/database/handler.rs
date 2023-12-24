fa647aa8b979cf890bda417d3f21628418be0760
image <image@by.cx>
2023-12-24 22:34:30 +0800

    replace file_url

--- src/database/handler.rs
+++ src/database/handler.rs
@@ -1,85 +1,80 @@
-pub mod handler{
+pub mod handler {
     use std::collections::HashMap;
     use std::time::SystemTime;
 
-    use mongodb::{Client, bson, Collection};
-    use mongodb::bson::{doc, Document};
-    use futures::stream::StreamExt;
+    use crate::entity::config::config::CONFIG;
     use crate::entity::image_detail::image_detail::ImageDetail;
-    use crate::entity::config::config::{ CONFIG};
+    use futures::stream::StreamExt;
+    use mongodb::bson::{doc, Document};
+    use mongodb::{bson, Client, Collection};
     extern crate redis;
     use redis::Commands;
-    static mut CLIENT:Option<Client> = None;
-    static mut REDIS_CLIENT:Option<redis::Client> = None;
+    static mut CLIENT: Option<Client> = None;
+    static mut REDIS_CLIENT: Option<redis::Client> = None;
 
-    pub fn get_redis() -> &'static Option<redis::Client>{
+    pub fn get_redis() -> &'static Option<redis::Client> {
         let redis_host;
-        let r_client ;
-        unsafe{
+        let r_client;
+        unsafe {
             redis_host = CONFIG.as_ref().unwrap().host.redis.as_str();
-            if REDIS_CLIENT.is_none(){
+            if REDIS_CLIENT.is_none() {
                 REDIS_CLIENT = Some(redis::Client::open(redis_host).expect("unable to open"));
             }
             r_client = &REDIS_CLIENT;
         }
         r_client
     }
-    pub fn redis_incr(){
+    pub fn redis_incr() {
         let r_client = get_redis();
         match r_client {
             Some(c) => {
                 let mut con = c.get_connection().expect("Unable to connecet redis");
-                let _ : () = con.incr("cv", 1).expect("incr count failed");
-            },
-            None => {
-
-            },
+                let _: () = con.incr("cv", 1).expect("incr count failed");
+            }
+            None => {}
         }
-        
     }
-    pub fn redis_incr_key(key: &str, value:usize){
+    pub fn redis_incr_key(key: &str, value: usize) {
         let r_client = get_redis();
         match r_client {
             Some(c) => {
                 let mut con = c.get_connection().expect("Unable to connecet redis");
-                let _ : () = con.incr(key, value).expect("incr count failed");
-            },
-            None => {
-
-            },
+                let _: () = con.incr(key, value).expect("incr count failed");
+            }
+            None => {}
         }
-        
     }
-    pub fn redis_get_value(key: &str) -> Option<u64>{
+    pub fn redis_get_value(key: &str) -> Option<u64> {
         let r_client = get_redis();
         match r_client {
             Some(c) => {
                 let mut con = c.get_connection().expect("Unable to connecet redis");
-                let v  = con.get(key).expect("failed get redis value");
+                let v = con.get(key).expect("failed get redis value");
                 v
-            },
-            None => {
-                None
-            },
+            }
+            None => None,
         }
-        
     }
-    pub fn call_count()->Option<u64>{
+    pub fn call_count() -> Option<u64> {
         let r_client = get_redis();
         match r_client {
             Some(c) => {
                 let mut con = c.get_connection().expect("Unable to connecet redis");
-                let cv  = con.get("cv").expect("failed get cv");
+                let cv = con.get("cv").expect("failed get cv");
                 cv
             }
-            None => {None},
+            None => None,
         }
     }
 
-    pub async fn image_count() -> Option<u64>{
-        unsafe{
-            if CLIENT.is_none(){
-                CLIENT = Some(Client::with_uri_str(CONFIG.as_ref().unwrap().system.mongo_uri.as_str()).await.unwrap());
+    pub async fn image_count() -> Option<u64> {
+        unsafe {
+            if CLIENT.is_none() {
+                CLIENT = Some(
+                    Client::with_uri_str(CONFIG.as_ref().unwrap().system.mongo_uri.as_str())
+                        .await
+                        .unwrap(),
+                );
             }
             match &CLIENT {
                 Some(client) => {
@@ -91,72 +86,88 @@ pub mod handler{
                                 "rating_on_ml":"s"
                             }
                         },
-                        doc!{
+                        doc! {
                             "$count":"source"
-                        }
+                        },
                     ];
-                    let mut cur = col.aggregate(pipeline,None).await.unwrap();
-                    if let Some(result) = cur.next().await{
+                    let mut cur = col.aggregate(pipeline, None).await.unwrap();
+                    if let Some(result) = cur.next().await {
                         match result {
-                            Ok(document)=>{
+                            Ok(document) => {
                                 return Some(document.get_i32("source").unwrap_or_default() as u64);
-                            },
-                            Err(_)=>{}
+                            }
+                            Err(_) => {}
                         }
-
                     }
                 }
-                None => {return None;},
+                None => {
+                    return None;
+                }
             }
-            return Some(0)
+            return Some(0);
         }
-        
     }
 
-    pub async fn last_time() -> bson::DateTime{
-        unsafe{
-            if CLIENT.is_none(){
-                CLIENT = Some(Client::with_uri_str(CONFIG.as_ref().unwrap().system.mongo_uri.as_str()).await.unwrap());
+    pub async fn last_time() -> bson::DateTime {
+        unsafe {
+            if CLIENT.is_none() {
+                CLIENT = Some(
+                    Client::with_uri_str(CONFIG.as_ref().unwrap().system.mongo_uri.as_str())
+                        .await
+                        .unwrap(),
+                );
             }
             match &CLIENT {
                 Some(client) => {
                     let db = client.database("anime");
                     let col: Collection<Document> = db.collection("artwork");
                     let pipeline = vec![
-                        doc!{
+                        doc! {
                             "$sort":{
                                 "created_at": -1i32
                             }
                         },
-                        doc!{
+                        doc! {
                             "$limit":1i32
-                        }
+                        },
                     ];
-                    let mut cur = col.aggregate(pipeline,None).await.unwrap();
-                    if let Some(result) = cur.next().await{
+                    let mut cur = col.aggregate(pipeline, None).await.unwrap();
+                    if let Some(result) = cur.next().await {
                         match result {
-                            Ok(document)=>{
+                            Ok(document) => {
                                 let ts = document.get_i32("created_at").unwrap() as i64;
-                                return bson::DateTime::from_millis(ts*1000);
-                            },
-                            Err(_)=>{return bson::DateTime::from_millis(0);}
+                                return bson::DateTime::from_millis(ts * 1000);
+                            }
+                            Err(_) => {
+                                return bson::DateTime::from_millis(0);
+                            }
                         }
                     }
-                },
-                None => {return bson::DateTime::from_millis(0);},
+                }
+                None => {
+                    return bson::DateTime::from_millis(0);
+                }
             }
             return bson::DateTime::from_millis(0);
         }
     }
 
-    pub async fn sample_one(id:Option<&String>, nin_tags:Option<Vec<&str>>, horizontal:Option<bool>, params: HashMap<String, String>) -> Option<ImageDetail> {
-        
+    pub async fn sample_one(
+        id: Option<&String>,
+        nin_tags: Option<Vec<&str>>,
+        horizontal: Option<bool>,
+        params: HashMap<String, String>,
+    ) -> Option<ImageDetail> {
         let start_time = SystemTime::now();
         redis_incr();
         let mut image = None;
         unsafe {
-            if CLIENT.is_none(){
-                CLIENT = Some(Client::with_uri_str(CONFIG.as_ref().unwrap().system.mongo_uri.as_str()).await.unwrap());
+            if CLIENT.is_none() {
+                CLIENT = Some(
+                    Client::with_uri_str(CONFIG.as_ref().unwrap().system.mongo_uri.as_str())
+                        .await
+                        .unwrap(),
+                );
             }
             let mut cursor;
             match &CLIENT {
@@ -164,78 +175,84 @@ pub mod handler{
                 Some(client) => {
                     let db = client.database("anime");
                     let col = db.collection("artwork");
-                    if id.is_some(){
-                        let find = doc!{
+                    if id.is_some() {
+                        let find = doc! {
                                 "_id":id.unwrap().parse::<u32>().unwrap()
                         };
                         cursor = col.find(find, None).await.unwrap();
-
-                    }else{
-                        let mut nin = vec!["nipples","nude","pussy","pussy_juice","uncensored","breasts"];
+                    } else {
+                        let mut nin = vec![
+                            "nipples",
+                            "nude",
+                            "pussy",
+                            "pussy_juice",
+                            "uncensored",
+                            "breasts",
+                        ];
                         if nin_tags.is_some() {
                             nin.extend(nin_tags.unwrap().iter());
                         }
-                        
+
                         let min = match params.get("min_size") {
-                            Some(v) => {
-                                v.parse::<u32>().unwrap_or(640)
-                            },
-                            None => {640},
+                            Some(v) => v.parse::<u32>().unwrap_or(640),
+                            None => 640,
                         };
                         let max = match params.get("max_size") {
-                            Some(v) => {
-                                v.parse::<u32>().unwrap_or(6144)
-                            },
+                            Some(v) => v.parse::<u32>().unwrap_or(6144),
                             None => 6144,
                         };
-                        let mut pipeline = vec![
-                            doc!{
-                                "$match":{
-                                    "rating_on_ml":"s",
-                                    "created_at":{"$gt":1506787200},
-                                    "file_size":{"$gt":500*1024, "$lt":12*1024*1024},
-                                    "$and":[
-                                        {"jpeg_width":{"$gt":min, "$lt":max}},
-                                        {"jpeg_height":{"$gt":min, "$lt":max}}
-                                    ]
-                                }
+                        let mut pipeline = vec![doc! {
+                            "$match":{
+                                "rating_on_ml":"s",
+                                "created_at":{"$gt":1506787200},
+                                "file_size":{"$gt":500*1024, "$lt":12*1024*1024},
+                                "$and":[
+                                    {"jpeg_width":{"$gt":min, "$lt":max}},
+                                    {"jpeg_height":{"$gt":min, "$lt":max}}
+                                ]
                             }
-                        ];
+                        }];
                         match horizontal {
                             Some(hor) => {
                                 let hor_value;
                                 if hor {
                                     hor_value = doc! {"$expr":{"$gt":["jpeg_width","jpeg_height"]}};
-                                }else{
+                                } else {
                                     hor_value = doc! {"$expr":{"$gt":["jpeg_height","jpeg_width"]}};
                                 }
                                 pipeline.push(hor_value);
-                            },
-                            None => {},
+                            }
+                            None => {}
                         }
-                        pipeline.push(doc!{
+                        pipeline.push(doc! {
                             "$sample":{
                                 "size":1
                             }
                         });
-                        cursor = col.aggregate(pipeline,None).await.unwrap();
+                        cursor = col.aggregate(pipeline, None).await.unwrap();
                     }
 
-                    if let Some(result) = cursor.next().await{
+                    if let Some(result) = cursor.next().await {
                         match result {
-                            Ok(document)=>{
-                                image = Some(bson::from_document(document).unwrap());
-                            },
-                            Err(_)=>{}
+                            Ok(document) => {
+                                let mut res: ImageDetail = bson::from_document(document).unwrap();
+                                res.file_url = CONFIG.as_ref().unwrap().host.domain[0].clone()
+                                    + "/"
+                                    + &res.md5[0..2]
+                                    + &res.md5
+                                    + &res.ext();
+                                image = Some(res);
+                            }
+                            Err(_) => {}
                         }
-
                     }
                 }
             }
-
         }
-        println!("mongo time: {:?}", SystemTime::now().duration_since(start_time).unwrap());
+        println!(
+            "mongo time: {:?}",
+            SystemTime::now().duration_since(start_time).unwrap()
+        );
         image
-
     }
 }
--- src/entity/image_detail.rs
+++ src/entity/image_detail.rs
@@ -1,17 +1,26 @@
-pub mod image_detail{
-    use serde::Serialize;
+pub mod image_detail {
     use serde::Deserialize;
-    #[derive(Debug,Serialize,Deserialize)]
-    pub struct ImageDetail{
-        pub(crate) file_url:String,
-        pub(crate) file_size:usize,
-        pub(crate) md5:String,
-        pub(crate) tags:Vec<String>,
-        pub(crate) width:u32,
-        pub(crate) height:u32,
-        pub(crate) source:String,
-        pub(crate) author:String,
-        pub(crate) has_children:bool,
-        pub(crate) _id:u32
+    use serde::Serialize;
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct ImageDetail {
+        pub(crate) file_url: String,
+        pub(crate) file_size: usize,
+        pub(crate) md5: String,
+        pub(crate) tags: Vec<String>,
+        pub(crate) width: u32,
+        pub(crate) height: u32,
+        pub(crate) source: String,
+        pub(crate) author: String,
+        pub(crate) has_children: bool,
+        pub(crate) _id: u32,
+    }
+    impl ImageDetail {
+        pub fn ext(&self) -> String {
+            self.file_url
+                .split(".")
+                .last()
+                .unwrap_or_default()
+                .to_string()
+        }
     }
 }
